be480969385930b4c24e51412845707b05b16827
Bohdan Petrenko <71459@student.wsiz.edu.pl>
2025-10-31 18:38:28 +0100

    Lab1 and Lab2 finished

---
+++ Lab1/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace Cwiczenie1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            void WypiszTekst()
+            {
+                Console.WriteLine("To jest cwiczenie 1");
+            }
+
+            WypiszTekst();
+
+            Zwierze a = new Zwierze();
+            Zwierze b = new Zwierze("Filemon", "Kot", 4);
+            Zwierze c = new Zwierze(b);
+
+            a.daj_glos();
+            b.daj_glos();
+            c.daj_glos();
+
+            Console.WriteLine($"Aktualna liczba zwierząt: {Zwierze.GetLiczbaZwierzat()}");
+        }
+    }
+
+    class Zwierze
+    {
+        private string nazwa;
+        private string gatunek;
+        private int liczbaNog;
+        private static int liczbaZwierzat = 0;
+
+        public string GetNazwa()
+        {
+            return nazwa;
+        }
+
+        public string GetGatunek()
+        {
+            return gatunek;
+        }
+
+        public int GetLiczbaNog()
+        {
+            return liczbaNog;
+        }
+
+        public void SetNazwa(string nowaNazwa)
+        {
+            nazwa = nowaNazwa;
+        }
+
+        public Zwierze()
+        {
+            nazwa = "Rex";
+            gatunek = "Pies";
+            liczbaNog = 4;
+            liczbaZwierzat++;
+        }
+
+        public Zwierze(string nazwa, string gatunek, int liczbaNog)
+        {
+            this.nazwa = nazwa;
+            this.gatunek = gatunek;
+            this.liczbaNog = liczbaNog;
+            liczbaZwierzat++;
+        }
+
+        public Zwierze(Zwierze other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException(nameof(other));
+            }
+
+            this.nazwa = other.nazwa;
+            this.gatunek = other.gatunek;
+            this.liczbaNog = other.liczbaNog;
+            liczbaZwierzat++;
+        }
+
+        public void daj_glos()
+        {
+            if (string.IsNullOrWhiteSpace(gatunek))
+            {
+                Console.WriteLine($"{nazwa} (nieznany gatunek): ...");
+                return;
+            }
+
+            switch (gatunek.Trim().ToLowerInvariant())
+            {
+                case "kot":
+                case "kotek":
+                case "kotka":
+                    Console.WriteLine($"{nazwa} ({gatunek}): Miau");
+                    break;
+                case "krowa":
+                    Console.WriteLine($"{nazwa} ({gatunek}): Muuu!");
+                    break;
+                case "pies":
+                case "piesek":
+                case "suczka":
+                    Console.WriteLine($"{nazwa} ({gatunek}): Hau!");
+                    break;
+                case "koń":
+                case "kon":
+                    Console.WriteLine($"{nazwa} ({gatunek}): Iiii-haaa!");
+                    break;
+                case "owca":
+                    Console.WriteLine($"{nazwa} ({gatunek}): Béé");
+                    break;
+                case "kura":
+                    Console.WriteLine($"{nazwa} ({gatunek}): Ko-ko-ryku!");
+                    break;
+                default:
+                    Console.WriteLine($"{nazwa} ({gatunek}): (brak znanego odgłosu)");
+                    break;
+            }
+        }
+
+        public static int GetLiczbaZwierzat()
+        {
+            return liczbaZwierzat;
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace Cwiczenie2
+{
+    abstract class Pracownik
+    {
+        public abstract void Pracuj();
+    }
+
+    class Piekarz : Pracownik
+    {
+        public override void Pracuj()
+        {
+            Console.WriteLine("Trwa pieczenie...");
+        }
+    }
+
+    class Zwierze
+    {
+        protected string nazwa;
+
+        public Zwierze(string nazwa)
+        {
+            this.nazwa = nazwa;
+        }
+
+        public virtual void daj_glos()
+        {
+            Console.WriteLine("...");
+        }
+    }
+
+    class Pies : Zwierze
+    {
+        public Pies(string nazwa) : base(nazwa) { }
+
+        public override void daj_glos()
+        {
+            Console.WriteLine($"{nazwa} robi woof woof!");
+        }
+    }
+
+    class Kot : Zwierze
+    {
+        public Kot(string nazwa) : base(nazwa) { }
+
+        public override void daj_glos()
+        {
+            Console.WriteLine($"{nazwa} robi miau miau!");
+        }
+    }
+
+    class Waz : Zwierze
+    {
+        public Waz(string nazwa) : base(nazwa) { }
+
+        public override void daj_glos()
+        {
+            Console.WriteLine($"{nazwa} robi ssssssss!");
+        }
+    }
+
+    class A
+    {
+        public A()
+        {
+            Console.WriteLine("To jest konstruktor A");
+        }
+    }
+
+    class B : A
+    {
+        public B() : base()
+        {
+            Console.WriteLine("To jest konstruktor B");
+        }
+    }
+
+    class C : B
+    {
+        public C() : base()
+        {
+            Console.WriteLine("To jest konstruktor C");
+        }
+    }
+
+    class Program
+    {
+        static void powiedz_cos(Zwierze z)
+        {
+            z.daj_glos();
+        }
+
+        public static void Main()
+        {
+            Zwierze z = new Zwierze("Zwierzak");
+            Pies p = new Pies("Azor");
+            Kot k = new Kot("Mruczek");
+            Waz w = new Waz("Kaa");
+
+            powiedz_cos(z);
+            Console.WriteLine($"Typ obiektu przekazanego jako parametr: {z.GetType().Name}");
+            powiedz_cos(p);
+            Console.WriteLine($"Typ obiektu przekazanego jako parametr: {p.GetType().Name}");
+            powiedz_cos(k);
+            Console.WriteLine($"Typ obiektu przekazanego jako parametr: {k.GetType().Name}");
+            powiedz_cos(w);
+            Console.WriteLine($"Typ obiektu przekazanego jako parametr: {w.GetType().Name}");
+
+            Piekarz piekarz = new Piekarz();
+            piekarz.Pracuj();
+
+            try
+            {
+                var obj = Activator.CreateInstance(typeof(Pracownik));
+                Console.WriteLine($"Utworzono instancję Pracownik: {obj}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Próba utworzenia instancji klasy abstrakcyjnej Pracownik zakończona błędem: {ex.Message}");
+            }
+
+            A a = new A();
+            B b = new B();
+            C c = new C();
+        }
+    }
+}
